8ecd5fbd8636aa5496b0449472f140af6759f85b
unknown <eliseychekoda001@gmail.com>
2023-10-17 18:12:05 +0300

    feat(header.jsx): header-component

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,7 +1,9 @@
+import { Header } from "../header";
+import "./app.css"
 export const App = () => {
   return (
     <div className="App">
-      <div>123</div>
+      <Header />
     </div>
   );
 };
---
+++ src/components/header/header.jsx
@@ -0,0 +1,21 @@
+import "./header.css"
+export const Header = () => {
+  return (
+    <div className="header">
+      test
+      <div className="header-menu">
+        <div className="menu-logo"></div>
+        <div className="menu-links"></div>
+        <div className="menu-phone"></div>
+      </div>
+      <div className="header-info">
+        <div className="header-info-consulate"></div>
+        <div className="header-info-image">
+          <div className="info-consulate-text"></div>
+          <div className="info-consulate-input"></div>
+          <div className="info-consulate-about"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
