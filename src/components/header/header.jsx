3811e1a4f5831fe2778ebe9c089822473cbb0080
unknown <eliseychekoda001@gmail.com>
2023-10-17 20:03:36 +0300

    feat(header.jsx, header.css): header-styles

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,19 +1,27 @@
-import "./header.css"
+import "./header.css";
 export const Header = () => {
   return (
     <div className="header">
-      test
-      <div className="header-menu">
-        <div className="menu-logo"></div>
-        <div className="menu-links"></div>
-        <div className="menu-phone"></div>
-      </div>
-      <div className="header-info">
-        <div className="header-info-consulate"></div>
-        <div className="header-info-image">
-          <div className="info-consulate-text"></div>
-          <div className="info-consulate-input"></div>
-          <div className="info-consulate-about"></div>
+      <div className="header-box">
+        <div className="header-menu">
+          <div className="menu-logo">L<div className="line-logo"></div>R</div>
+          <div className="menu-links">
+            <p>Главная</p>
+            <p>Услуги</p>
+            <p>Сопровождение</p>
+            <p>Полезное</p>
+            <p>О компании</p>
+            <p>Контакты</p>
+          </div>
+          <div className="menu-phone">+7 (999) 890 09 98</div>
+        </div>
+        <div className="header-info">
+          <div className="header-info-consulate"></div>
+          <div className="header-info-image">
+            <div className="info-consulate-text"></div>
+            <div className="info-consulate-input"></div>
+            <div className="info-consulate-about"></div>
+          </div>
         </div>
       </div>
     </div>
