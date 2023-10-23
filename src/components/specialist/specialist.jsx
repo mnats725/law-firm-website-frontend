5fb80f2a59f8712158b9b259ab321a03c2f1c9e2
unknown <eliseychekoda001@gmail.com>
2023-10-23 21:31:29 +0300

    feat(specialist.jsx,specialist.css): specialist-block

--- src/components/specialist/specialist.jsx
+++ src/components/specialist/specialist.jsx
@@ -25,6 +25,7 @@ export const Specialist = () => {
           <button>Заказать звонок</button>
         </div>
         <div className="specialist-persones">
+
           <div className="persones-guy">
             <div className="persones-picture">
               <img src={Guy1}></img>
@@ -34,31 +35,34 @@ export const Specialist = () => {
               <h2>32 лет стажа</h2>
             </div>
           </div>
+
           <div className="persones-guy-v2">
-            <div className="persones-picture">
-              <img src={Guy2}></img>
-            </div>
-            <div className="persones-text">
-              <h1>Любовть Юрьевна</h1>
+            <div className="persones-text-v2">
+              <h1>Любовь Юрьевна</h1>
               <h2>18 лет стажа</h2>
+              <div className="persones-picture">
+                <img src={Guy2}></img>
+              </div>
             </div>
           </div>
+
           <div className="persones-guy">
             <div className="persones-picture">
               <img src={Guy3}></img>
             </div>
             <div className="persones-text">
               <h1>Иван Васильевич</h1>
-              <h2></h2>
+              <h2>17 лет стажа</h2>
             </div>
           </div>
+
           <div className="persones-guy-v2">
-            <div className="persones-picture-v2">
-              <img src={Guy4}></img>
-            </div>
-            <div className="persones-text-v3">
-              <h1>Анна Дм</h1>
+            <div className="persones-text-v2">
+              <h1>Анна</h1>
               <h2>12 лет</h2>
+              <div className="persones-picture-v2">
+                <img src={Guy4}></img>
+              </div>
             </div>
           </div>
         </div>
