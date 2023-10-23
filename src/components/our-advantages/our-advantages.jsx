88e86de925046e462e0e226abb818c8b4725c57b
unknown <eliseychekoda001@gmail.com>
2023-10-23 14:20:56 +0300

    style(our-advantages.jsx): our-advantages-delete-superfluous

--- src/components/our-advantages/our-advantages.jsx
+++ src/components/our-advantages/our-advantages.jsx
@@ -4,11 +4,6 @@ import { AdvantagesWhy } from "./advantages-why";
 import { AdvantagesWhyBlocks } from "./advantages-why-blocks";
 import { AdvantagesButton } from "./advantages-button";
 
-import FullTime from "../../assets/images/full-time.png";
-import Prof from "../../assets/images/prof.png";
-import Think from "../../assets/images/think.png";
-import Mind from "../../assets/images/mind.png";
-
 export const OurAdvantages = () => {
   return (
     <div className="our-advantages-container">
