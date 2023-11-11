e476e77caed87dafef9d4cdf0480f0fd8672e2ab
unknown <eliseychekoda001@gmail.com>
2023-11-11 15:37:45 +0300

    feat(info-contacts.jsx,): info-contacts-component

---
+++ src/components/info-contacts/info-contacts.jsx
@@ -0,0 +1,11 @@
+import "./info-contacts.css";
+
+export const InfoContacts = () => {
+  return (
+    <div className="info-contacts-container">
+        <div className ="info-contacts">
+            
+        </div>
+    </div>
+  );
+};
